c8d1c0bbd42edab8516ac77c7603b9002bf76b29
raraku <airenov500@gmail.com>
2021-10-30 17:43:31 +0100

    wash

--- src/Display/Web/Pages/RandomManga/RandomManga.js
+++ src/Display/Web/Pages/RandomManga/RandomManga.js
@@ -86,11 +86,11 @@ const RandomManga = (props) => {
   return (
     <div>
       <Helmet>
-        <title>Random Manga - EliteManga</title>
+        <title>Random Manga & Anime Recommendation - EliteManga</title>
         <meta
           name="description"
           content="Don't know what to read next? Click the button and our systems will
-          generate a random Manga for you. Don't like it? Click the button
+          recommend a random manga and anime for you for you. Don't like it? Click the button
           again!"
         />
       </Helmet>
